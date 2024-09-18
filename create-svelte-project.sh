edb4ec7d6863910ebd251fc2d51fe1f772cd0b4d
Mark Burggraf <markb@mantisbible.com>
2024-09-18 14:08:20 -0700

    added constants from package.json

--- create-svelte-project.sh
+++ create-svelte-project.sh
@@ -49,8 +49,28 @@ cat > vite.config.ts << EOL
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { fileURLToPath, URL } from 'node:url'
+import { readFileSync } from "node:fs";
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 export default defineConfig({
+	define: {
+		__DATE__: \`'\${new Date().toISOString()}'\`,
+		__RELOAD_SW__: false,
+		'process.env.NODE_ENV': process.env.NODE_ENV === 'production' ? '"production"' : '"development"',
+		'__APP_VERSION__': JSON.stringify(pkg.version),
+		'__APP_NAME__': JSON.stringify(pkg.name),
+		'__APP_HOMEPAGE__': JSON.stringify(pkg.homepage),
+		'__APP_DESCRIPTION__': JSON.stringify(pkg.description),
+		'__APP_MENU_TITLE__': JSON.stringify(pkg.menu_title),
+		'__APP_MENU_SUBTITLE__': JSON.stringify(pkg.menu_subtitle),
+		'__APP_PROFILE_TABLE__': JSON.stringify(pkg.profileTable),
+		'__APP_PROFILE_KEY__': JSON.stringify(pkg.profileKey),
+		'__APP_THEME_COLOR__': JSON.stringify(pkg.theme_color),
+		'__APP_BACKGROUND_COLOR__': JSON.stringify(pkg.background_color),	  
+	},
   plugins: [svelte()],
   resolve: {
     alias: {
